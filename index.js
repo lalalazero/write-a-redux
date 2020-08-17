96dc7b8d082301cd7501700a1c14f45028198b84
lalalazero <zzzero520@hotmail.com>
2020-08-17 15:29:36 +0800

    3. combineReducer

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
+function combineReducer(obj){
+    return function mergedReducer(state, action){
+        let nextState = state 
+        let keys = Object.keys(obj)
+        for(let i = 0; i < keys.length; i++){
+            const nextKey = keys[i]
+            const nextReducer = obj[nextKey]
+            const nextStateForKey = nextReducer(nextState[nextKey], action)
+            nextState[nextKey] = nextStateForKey
+        }
+        return nextState
+    }
+}
+
 function createStore(initState, reducer) {
     let state = initState
     let listeners = []
@@ -16,20 +30,26 @@ function createStore(initState, reducer) {
             listeners[i]()
         }
     }
+    dispatch({ type: Symbol() }) // 获取每个 reducer 返回的 default state
     return {
         getState,
         subscribe,
         dispatch
     }
 }
-function reducer(state, action) {
+function countReducer(count = 0, action) {
     switch (action.type) {
-        case 'increment': return { count: state.count + action.payload };
-        case 'decrement': return { count: state.count - action.payload };
-        default: return state
+        case 'increment': return count + action.payload;
+        case 'decrement': return count - action.payload;
+        default: return count
     }
 }
-let store = createStore({ count: 0 }, reducer)
+
+let mergedReducer = combineReducer({
+    count: countReducer
+})
+
+let store = createStore({}, mergedReducer)
 function fn() {
     console.log('state has change')
     console.log(store.getState())
@@ -37,3 +57,5 @@ function fn() {
 store.subscribe(fn)
 store.dispatch({ type: 'increment', payload: 12 })
 store.dispatch({ type: 'decrement', payload: 6 })
+store.dispatch({ type: 'increment', payload: 2 })
+store.dispatch({ type: 'decrement', payload: 4 })
